34e8b877dd262413d19e2f691a589bf8109c7d59
qjex <theqjex@gmail.com>
2018-06-10 15:06:21 +0300

    list updated

--- list/list.h
+++ list/list.h
@@ -205,14 +205,6 @@ private:
     void swap(list<T> &a, list<T> &b) {
         std::swap(a.fake, b.fake);
 
-        if (b.tail->l == a.tail) {
-            b.tail->l = b.tail->r = &b.fake;
-        }
-
-        if (a.tail->l == b.tail) {
-            a.tail->l = a.tail->r = &a.fake;
-        }
-
         a.tail->l->r = &a.fake;
         a.tail->r->l = &a.fake;
 
